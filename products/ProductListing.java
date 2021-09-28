e2885f3f9ec3b6e50fcb47beb78a8fbffc36c637
Muhammad-Nasofi <91511329+Muhammad-Nasofi@users.noreply.github.com>
2021-09-28 08:56:39 +0700

    Create ProductListing.java

---
+++ products/ProductListing.java
@@ -0,0 +1,8 @@
+package products;
+
+public class ProductListing {
+    public static void main(String[] args) {
+        Product product_a = new Product("product a");
+        product_a.getProductListing(5, 15, 10);
+    }
+}
